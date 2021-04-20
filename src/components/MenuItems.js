ec272bc3b69cd4f2dcbad20ac7537d7c32de8b6d
MeyerFrenchel <meyer.frenchel@gmail.com>
2021-04-20 22:39:20 +0300

    navbar first touches from home

---
+++ src/components/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+  {
+    title: 'Despre clientii nostrii',
+    path: '/marketing',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Echipa Contabilitate',
+    path: '/consulting',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Avantake Clienti Contabilitate',
+    path: '/design',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Servicii Contabilitate',
+    path: '/development',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Tarife',
+    path: '/development',
+    cName: 'dropdown-link'
+  }
+];
---
+++ src/components/Navbar.js
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import Dropdown from './Dropdown';
+import LogoPic from '../images/logo.png';
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const onMouseEnter = () => {
+    if (window.innerWidth < 765) {
+      setDropdown(false);
+    } else {
+      setDropdown(true);
+    }
+  };
+
+  const onMouseLeave = () => {
+    if (window.innerWidth < 765) {
+      setDropdown(false);
+    } else {
+      setDropdown(false);
+    }
+  };
+
+  return (
+    <>
+      <nav className='navbar'>
+        <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+          <img className='logo-pic' src={LogoPic} alt="logo"/>
+          
+        </Link>
+        <div className='menu-icon' onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Infiintari Firme
+            </Link>
+          </li>
+          <li
+            className='nav-item'
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+          >
+            <Link
+              to='/contabilitate'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              Contabilitate si Salarizare <i className='fas fa-caret-down' />
+            </Link>
+            {dropdown && <Dropdown />}
+          </li>
+          <li className='nav-item'>
+            <Link
+              to='/products'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              Traduceri
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link
+              to='/contact-us'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              Despre noi
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link
+              to='/contact-us'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link
+              to='/sign-up'
+              className='nav-links-mobile'
+              onClick={closeMobileMenu}
+            >
+              Sign in
+            </Link>
+          </li>
+        </ul>
+        <div className="nav-btn-container">
+          <i className="fas fa-cart-plus  fa-2x"></i>
+          <Button title={'Sign In'}/>
+          <Button title={'Sign Out'}/>
+        </div>
+
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
